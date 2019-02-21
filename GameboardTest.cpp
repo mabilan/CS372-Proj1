c2b21c0b84b9cffedcc82dff0254d8ecee7308ef
Bryant <bgklug@alaska.edu>
2019-02-20 23:17:23 -0900

    Create testing file for Gameboard class

---
+++ GameboardTest.cpp
@@ -0,0 +1,13 @@
+// GameboardTest.cpp
+// Created on 2/20/2019 by Bryant Klug
+//   last modified: 2/20/2019
+
+#include "catch.hpp"
+
+TEST_CASE("Testing the Gameboard class", "[gameboard]"){
+    Gameboard testBoard;
+    SECTION("Gameboard has a size"){
+        REQUIRE(testBoard.getHeight() == 10);
+        REQUIRE(testBoard.getWidth() == 10);
+
+}
